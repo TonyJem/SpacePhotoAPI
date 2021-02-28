1045a8252d22ba67c8ef0fd4ef4fd12d92fccfbe
TonyJem <a.jemanov@gmail.com>
2021-02-28 19:40:47 +0200

    Enable to fetch Picture

--- SpacePhotoAPI/SpacePhotoAPI/ViewController.swift
+++ SpacePhotoAPI/SpacePhotoAPI/ViewController.swift
@@ -14,11 +14,21 @@ class ViewController: UIViewController {
         
         photoInfoController.fetchPhotoInfo { (photoInfo) in
             guard let photoInfo = photoInfo else { return }
-            DispatchQueue.main.async {
-                self.title = photoInfo.title
-                self.descriptionLabel.text = photoInfo.description
-            }
+            self.updateUI(with: photoInfo)
         }
     }
     
+    func updateUI(with photoInfo: PhotoInfo) {
+        
+        let task = URLSession.shared.dataTask(with: photoInfo.url) {(data, response, error) in
+            guard let data = data,
+                  let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self.title = photoInfo.title
+                self.photoImageView.image = image
+                self.descriptionLabel.text = photoInfo.description
+            }
+        }
+        task.resume()
+    }
 }
